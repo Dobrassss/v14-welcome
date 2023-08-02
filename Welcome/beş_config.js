f105a826d313133aac821204824c884b8053a86f
Dobrassss <126970840+Dobrassss@users.noreply.github.com>
2023-08-02 19:10:32 +0300

    v14-welcome
    altyapı bana ait değildir

---
+++ Welcome/beş_config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presence: "server <3 Felis Dobra", // Bot Durumu
+  guildID: "1098202443466158120", // Sunucu ID'si
+  mp3: true, // mp3 mü Yoksa Youtube Üzerindenmi Çalmasını İsterseniz Burdan true veya false Yazın.
+  file: "./ses.mp3", // mp3'ü Seçtiyseniz Ses Dosyasının Uzantısı
+  staffRole: "1136324878249775184",//En Alt Yetkili Rol ID'si
+  youtubeURL: "https://www.youtube.com/", // Youtube'u Seçtiyseniz Bir Youtube URL'si
+  tokens: [ // Tokenler ["55555","5555"] Şeklinde Giriniz.
+    "MTEzNjMyNjE2MjY5NjY1MDk0Mw.GqyCH9.1tESVGQhCG48xhFCbxtVziTkG4pDvMI1u53jhg",
+    "MTEzNjMyNjE5MTgwOTMxMDgzMg.G7fKWm.GK4zaBM5H2Sdla_YjFsZmkVXWN1RABM8aJht98",
+    "MTEzNjMyNjIyMTc1MjQzODkyNA.GRblLs.19bEE4mPqV1rLSp23vsGsH08zTdObi_BEmFwh0",
+    "MTEzNjMyNjI0NDA1Nzc1MTYyMg.G_2TBG.icQ7kpMrihjLvZCpouyDd8JfGrGBi1yxoIgx9g"
+  ],
+  channels: [ // Welcome Kanallarının ID'leri ["55555","5555"] Şeklinde Giriniz.
+    "1098202444107886651",
+    "1098202444107886650",
+    "1098202444107886649",
+    "1098202444107886648"
+  ]
+}
